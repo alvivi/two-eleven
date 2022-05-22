6947c08e6d652d3c1753258164f254ba94070b10
Alvaro Vilanova Vidal <alvivi@gmail.com>
2022-05-22 23:30:40 +0200

    Check accounts context

---
+++ apps/two_eleven/test/two_eleven/accounts_test.exs
@@ -0,0 +1,37 @@
+defmodule TwoEleven.AccountsTest do
+  use ExUnit.Case
+
+  alias TwoEleven.{Accounts, Accounts.Player}
+
+  setup :reset_accounts_database
+
+  describe "new_player/1" do
+    test "creates a fetchable player" do
+      assert {:ok, player} = Accounts.new_player()
+      assert {:ok, player} == Accounts.fetch_player(player.id, player.token)
+    end
+
+    test "returns an error if the maximum number of retries is reached" do
+      assert :error = Accounts.new_player(-1)
+    end
+  end
+
+  describe "fetch_player/2" do
+    test "fetches a created player" do
+      assert {:ok, player} = Accounts.new_player()
+      expected_player = %Player{id: player.id, name: player.name, token: player.token}
+      assert {:ok, expected_player} == Accounts.fetch_player(player.id, player.token)
+    end
+
+    test "returns an error if the user does not exists" do
+      assert :error == Accounts.fetch_player("foo", "bar")
+    end
+
+    test "returns an error when token is invalid" do
+      assert {:ok, player} = Accounts.new_player()
+      assert :error == Accounts.fetch_player(player.id, "foo")
+    end
+  end
+
+  defp reset_accounts_database(_context), do: Accounts.init(reset: true)
+end
